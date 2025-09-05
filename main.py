0c51e6799b6eec9f9cdbbed6693bb16a70f7c699
best200-lab <ogunseun7@gmail.com>
2025-09-05 10:56:43 +0100

    first commit

--- main.py
+++ main.py
@@ -236,4 +236,4 @@ async def get_chat(chat_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
